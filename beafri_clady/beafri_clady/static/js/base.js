ce3a0489a56e34b2e00edbe1df22a786130796e8
Fred <fngarimwaniki.rc@gmail.com>
2024-05-28 11:42:35 +0300

    Fresh

--- beafri_clady/beafri_clady/static/js/base.js
+++ beafri_clady/beafri_clady/static/js/base.js
@@ -2,19 +2,13 @@ function toggleSidebar() {
     var sidebar = document.getElementById("sidebar");
     var mainContent = document.getElementById("main-content");
     var toggleIcon = document.getElementById("toggle-icon");
-    var openBtn = document.querySelector(".open-btn");
 
     if (sidebar.style.left === "0px") {
         sidebar.style.left = "-250px";
         mainContent.style.marginLeft = "0";
-        openBtn.style.opacity = "0"; 
-        sidebar.addEventListener('transitionend', function() {
-            openBtn.style.opacity = "1"; 
-        });
     } else {
         sidebar.style.left = "0px";
         mainContent.style.marginLeft = "250px";
-        openBtn.style.opacity = "1"; 
     }
 }
 
@@ -26,7 +20,7 @@ window.onclick = function(event) {
         document.getElementById("main-content").style.marginLeft = "0";
         openBtn.style.opacity = "0";
         sidebar.addEventListener('transitionend', function() {
-            openBtn.style.opacity = "1"; 
+            openBtn.style.opacity = "1";
         });
     }
 }
@@ -39,11 +33,10 @@ function adjustSidebarHeight() {
     const authLinksHeight = authLinks.offsetHeight;
     const sidebarHeight = window.innerHeight;
 
-    sidebarContent.style.height = `${sidebarHeight - authLinksHeight - 60}px`; 
+    sidebarContent.style.height = `${sidebarHeight - authLinksHeight - 60}px`;
     sidebarContent.style.overflowY = 'auto';
 }
 
 window.addEventListener('resize', adjustSidebarHeight);
 window.addEventListener('load', adjustSidebarHeight);
 document.addEventListener('DOMContentLoaded', adjustSidebarHeight);
-
